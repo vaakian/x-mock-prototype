689483632518abf60054e24ac5f2ada72a7d7f3c
vaakian <vaakian@gmail.com>
2024-03-02 01:21:13 +0800

    refactor: extract `BaseRule` type

--- src/msw/type.ts
+++ src/msw/type.ts
@@ -3,19 +3,19 @@ export const nativeFetch = window.fetch;
 
 export type Awaitable<T> = T | Promise<T>;
 export type Matcher = (request: Request) => boolean;
+export type BaseRule<T> = {
+  matcher: Matcher;
+  enabled: boolean;
+  delay?: number;
+} & T;
+
 export type Rule =
-  | {
-      matcher: Matcher;
-      enabled: boolean;
-      delay?: number;
+  | BaseRule<{
       response: string;
-    }
-  | {
-      matcher: Matcher;
-      enabled: boolean;
-      delay?: number;
+    }>
+  | BaseRule<{
       handler: (response: string) => Awaitable<string>;
-    };
+    }>;
 
 export type InterceptionDescriptor = {
   url: string;
